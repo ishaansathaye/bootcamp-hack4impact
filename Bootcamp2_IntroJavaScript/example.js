1127395300f9bd6f1421f55fa3469d48748e9297
ishaansathaye <ishaan.sathaye@gmail.com>
2021-10-21 19:14:26 -0700

    update

--- Bootcamp2_IntroJavaScript/example.js
+++ Bootcamp2_IntroJavaScript/example.js
@@ -1,3 +1,7 @@
 let prices = [5.00, 8.00, 12.00, 1.15, 15.00, 4.00]
 prices.map(x => x*0.8 == x < 10)
 console.log(prices)
+
+return prices
+    .map(price => price < 10 ? price*0.8 == price)
+    .filter(price => price >= 2);
